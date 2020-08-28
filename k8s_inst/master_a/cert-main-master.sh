724fedb6080a8a53dec2444c8ba120bdc9044b08
lilzhang90s <lilzhang90s@163.com>
2020-08-28 20:53:58 +0800

    add master cfg

---
+++ k8s_inst/master_a/cert-main-master.sh
@@ -0,0 +1,13 @@
+USER=root
+CONTROL_PLANE_IPS="10.8.0.15 10.8.0.23"
+for host in ${CONTROL_PLANE_IPS}; do
+    scp /etc/kubernetes/pki/ca.crt "${USER}"@$host:
+    scp /etc/kubernetes/pki/ca.key "${USER}"@$host:
+    scp /etc/kubernetes/pki/sa.key "${USER}"@$host:
+    scp /etc/kubernetes/pki/sa.pub "${USER}"@$host:
+    scp /etc/kubernetes/pki/front-proxy-ca.crt "${USER}"@$host:
+    scp /etc/kubernetes/pki/front-proxy-ca.key "${USER}"@$host:
+    scp /etc/kubernetes/pki/etcd/ca.crt "${USER}"@$host:etcd-ca.crt
+    # Quote this line if you are using external etcd
+    scp /etc/kubernetes/pki/etcd/ca.key "${USER}"@$host:etcd-ca.key
+done
